28fdbe255407685307f45b9cfba4f5dc3367b44a
jeffreyz69 <jeffreyz1234@gmail.com>
2023-09-17 21:53:15 -0400

    makefle and more testing with features

---
+++ Makefile
@@ -0,0 +1,7 @@
+setup: 
+	
+	conda activate p4900
+
+
+run: 
+	streamlit run /Users/jeffreyzhang/Projects/CISC4900/src/testlit.py
--- src/testlit.py
+++ src/testlit.py
@@ -7,6 +7,11 @@ st.write("""
 # My first app
 Hello *world!*
 """)
+         
+text_search = st.text_input('Search')
+
+#test to see if the input is read correctly
+st.write('You searched for: ', text_search)
  
 df = pd.read_csv("data/my_data.csv")
 st.line_chart(df)
