85675763a8e4452225ca504ee24a172efa7613f5
jeffreyz69 <jeffreyz1234@gmail.com>
2023-10-10 14:29:59 -0400

    next stage of topics/subtopics implementation

--- src/App.py
+++ src/App.py
@@ -1,46 +1,44 @@
 import os
 import streamlit as st
 import pandas as pd
-
 from Topics import Topics
-#import sqlite3
+from SubTopics import SubTopics  # Import the SubTopics class
 
-# create a array of Topics objects of size n
-topics = []
-
-# read csv file and the first word of each line is the topic id ignoring the comma. 
-# the second word of each line is the topic name.
-# Using pd.read_csv() to read the csv file.
-# Then make a new Topics object and add id and name to the object. Topics(id, name)
-df = pd.read_csv('../data/my_data.csv', engine='python', header=None, delimiter=', ')
-topic_ids = set()
-for index, row in df.iterrows():
-    if row[0] not in topic_ids:
-        topic = Topics(row[0], row[1])
-        topics.append(topic)
-        topic_ids.add(row[0])
-
-
-# print the topics to console for testing
-for topic in topics:
-    print(topic.toString())
-
-
- 
 print('running from ' + os.getcwd())
 st.write("""# My first app
 Hello *world!*
 """)
-         
+
+# Create an array of Topics objects
+topics = []
+
+# Read the CSV file and group subtopics into topics
+df = pd.read_csv('../data/my_data.csv', engine='python', header=None, delimiter=', ')
+topic_dict = {}  # Dictionary to store topics and their subtopics
+
+for index, row in df.iterrows():
+    topic_id, topic_name, subtopic_name = row[0], row[1], row[2]
+
+    if topic_id not in topic_dict:
+        topic_dict[topic_id] = Topics(topic_id, topic_name)
+    
+    subtopic = SubTopics(topic_id, subtopic_name)
+    topic_dict[topic_id].addSubTopic(subtopic)
+
+# Convert the dictionary values (topics) to a list
+topics = list(topic_dict.values())
+
+# Print the topics and their subtopics to the web page using streamlit
+for topic in topics:
+    st.write(topic.toString())
+    for subtopic in topic.getSubTopics():
+        st.write(subtopic.toString())
+
 text_search = st.text_input('Search')
 
-#test to see if the input is read correctly
+# Test to see if the input is read correctly
 st.write('You searched for: ', text_search)
 
-# print from csv file. 
+# Print data from the CSV file.
 df = pd.read_csv('../data/my_data.csv')
 st.write(df)
-
-# reads csv file and puts it into a array only 
-
-
--- src/Topics.py
+++ src/Topics.py
@@ -11,12 +11,24 @@ class Topics:
     def __init__(self, topic_id, topic_name):
         self.topic_id = topic_id
         self.topic_name = topic_name
+        self.subtopics = [] # initialize a array of subtopics
+    
+    # get topic id
+    def getID(self):
+        return self.topic_id
+    # get topic name
+    def getName(self):
+        return self.topic_name
     
     # to string method
     def toString(self):
-        return "Topic ID: (" + str(self.topic_id) + ")Topic Name: " + str(self.topic_name)
+        return f"Topic ID: ({self.topic_id}) Topic Name: {self.topic_name}"
     
     # add subtopic to array
     def addSubTopic(self, subtopic):
         self.subtopics.append(subtopic)
+    
+    # get topics and its subtopics
+    def getSubTopics(self):
+        return self.subtopics
     
