e1af7b672919bba7d30c72af2b1b8254a5015e98
jeffreyz69 <jeffreyz1234@gmail.com>
2023-10-10 18:13:58 -0400

    got 2d array working

--- src/App.py
+++ src/App.py
@@ -2,17 +2,13 @@ import os
 import streamlit as st
 import pandas as pd
 from Topics import Topics
-from SubTopics import SubTopics  # Import the SubTopics class
 
 print('running from ' + os.getcwd())
 st.write("""# My first app
 Hello *world!*
 """)
-
-# Create an array of Topics objects
 topics = []
 
-# Read the CSV file and group subtopics into topics
 df = pd.read_csv('../data/my_data.csv', engine='python', header=None, delimiter=', ')
 topic_dict = {}  # Dictionary to store topics and their subtopics
 
@@ -20,10 +16,8 @@ for index, row in df.iterrows():
     topic_id, topic_name, subtopic_name = row[0], row[1], row[2]
 
     if topic_id not in topic_dict:
-        topic_dict[topic_id] = Topics(topic_id, topic_name)
-    
-    subtopic = SubTopics(topic_id, subtopic_name)
-    topic_dict[topic_id].addSubTopic(subtopic)
+        topic_dict[topic_id] = Topics(topic_id, topic_name) # Create a new Topics object
+    topic_dict[topic_id].addSubTopic(subtopic_name)  # Add subtopic to the corresponding Topics object
 
 # Convert the dictionary values (topics) to a list
 topics = list(topic_dict.values())
@@ -32,7 +26,7 @@ topics = list(topic_dict.values())
 for topic in topics:
     st.write(topic.toString())
     for subtopic in topic.getSubTopics():
-        st.write(subtopic.toString())
+        st.write(subtopic)
 
 text_search = st.text_input('Search')
 
