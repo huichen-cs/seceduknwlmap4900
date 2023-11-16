d498e5d440edc372ad5181b7759dfdf0fbf1b7cc
jeffreyz69 <jeffreyz1234@gmail.com>
2023-11-16 00:44:36 -0500

    Fixed bug where the CVE section was not populated. Still need to fix bug for table sizing and multiple elements being in same table element when it should be below.

--- src/pages/AppPage3.py
+++ src/pages/AppPage3.py
@@ -22,17 +22,17 @@ df2 = pd.read_csv('../data/my_data2.csv', engine="python" , names = colNames2 ,h
 
 # Table object has these parameters: knowledgeArea, knowledgeTopic, CWE des, CWEID, sourceURLcwe, cve des, cveID, sourceURLcve
 # traverse through the first csv file and add the values that matches the knowledgeArea, knowledgeTopic, CWE des, CWEID, sourceURLcwe
-for index, row in df.iterrows():
-    knowledgeArea = row['Topic Name']
-    knowledgeTopic = row['SubTopic Name']
-    CWE = row['Weakness Name']
-    CWEID = row['CWE ID']
-    sourceURLcwe = row['Source URL']
-    # cve = ""
-    # cveID = ""
-    # sourceURLcve = ""
+# for index, row in df.iterrows():
+#     knowledgeArea = row['Topic Name']
+#     knowledgeTopic = row['SubTopic Name']
+#     CWE = row['Weakness Name']
+#     CWEID = row['CWE ID']
+#     sourceURLcwe = row['Source URL']
+#     cve = ""
+#     cveID = ""
+#     sourceURLcve = ""
 
-    # traverse through the first csv file and add the values that match
+# traverse through the first csv file and add the values that match
 for index, row in df.iterrows():
     knowledgeArea = row['Topic Name']
     knowledgeTopic = row['SubTopic Name']
@@ -46,10 +46,14 @@ for index, row in df.iterrows():
 
     # traverse through the second csv file and add the values that match
     for index2, row2 in df2.iterrows():
-        if CWE == row2['Software vulnerability name (CVE name)']:
-            cve.append(row2['cve Description'])
-            cveID.append(row2['cve num'])
-            sourceURLcve.append(row2['sourceURLcve'])
+        # if the CWEID mathces the CVE's CWEID located in last row 'CWE ID', add the values to the cve array
+        # ignore case sensitivity and strip the whitespace
+        if str(row2['CWE ID']).strip().lower() == str(CWEID).strip().lower():
+            cve.append(row2['Software vulnerability name (CVE name)'])
+            cveID.append(row2['CVE ID'])
+            sourceURLcve.append(row2['Source URL'])
+
+        
 
     # add the table to the array of tables
     tables.append(Table(knowledgeArea, knowledgeTopic, CWE, CWEID, sourceURLcwe, cve, cveID, sourceURLcve))
